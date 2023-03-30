dc90d5bc8a3539b75a435571b1923bcca8e2bd0e
ArjaniAleena <118487445+ArjaniAleena@users.noreply.github.com>
2023-03-30 17:56:40 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+var  apple, appleImg
+var wood, woodImg, crackedWood, crackedWoodImg, crackedWood2, crackedWood2Img
+var crossSword, crossedSwordImg
+var fullSword, fullSwordImg, remainingSword, remainingSwordImg
+var sword, swordImg
+var background,  backgroundImg
+
+function preload(){
+   
+appleImg = loadImage("assets/apple.png")
+woodImg = loadImage("assets/fullWood.png")
+crackedWoodImg = loadImage("assets/crackedWood.png")
+crackedWood2Img = loadImge("assets/crackedWood2.png")
+crossedSwordImg = loadImage("assets/crossedSwords.png")
+fullSwordImg = loadImage("assets/fullSword.png")
+remainingSwordImg = loadImage("assets/remainingSword.png")
+swordImg = loadImage("assets/sword.png")
+backgroundImg = loadImage("assets/woodenbg.jpg")
+
+}
+
+
+function setup(){
+createCanvas(600,840)
+
+}
+
+function draw(){
+background("purple")
+}
